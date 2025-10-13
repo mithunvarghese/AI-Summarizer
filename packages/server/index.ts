eaafc04cadd3b42cc1b1a6953233f6322db2aff0
vargis.mithun <varghese.mithun93@gmail.com>
2025-10-13 09:41:45 +0530

    Project Set up

---
+++ packages/server/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import dotenv from 'dotenv';
+import router from './routes';
+
+
+dotenv.config();
+
+
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+
+app.listen(3000, () => {
+    console.log("Here it started")
+})
