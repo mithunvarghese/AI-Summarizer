a0f8503fb5fb9e66baa2237b233b594161c11008
vargis.mithun <varghese.mithun93@gmail.com>
2025-10-18 21:32:04 +0530

    Add routes and contrller

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,5 +1,23 @@
-import express from 'express';
+import express, { type Request, type Response } from 'express';
+import { PrismaClient } from './generated/prisma';
 
 const router = express.Router();
+
+
+router.get("/api/products/:id/reviews", async (req: Request, res: Response) => {
+    const client = new PrismaClient();
+    const productId = Number(req.params?.id);
+
+    if(isNaN(productId)) 
+        res.status(400).json({error: "Invalid Product Id"})
+
+    const reviews =  await client.review.findMany({
+        where: {productId},
+        orderBy: {createdAt: 'desc'}
+    })
+
+    res.json(reviews);
+
+})
 
 export default router;
